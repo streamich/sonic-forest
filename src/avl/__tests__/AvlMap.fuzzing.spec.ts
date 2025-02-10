4acced46c4e8c47804d94e794033ab488ea5915e
streamich <streamich@gmail.com>
2025-02-10 15:01:19 +0100

    test: 💍 add fuzz testing

---
+++ src/avl/__tests__/AvlMap.fuzzing.spec.ts
@@ -0,0 +1,12 @@
+import {MapFuzzer} from '../../__tests__/MapFuzzer';
+import {AvlMap} from '../AvlMap';
+
+describe('AvlMap fuzzing', () => {
+  for (let i = 0; i < 25; i++) {
+    test(`map instance ${i}`, () => {
+      const map = new AvlMap<number, number>();
+      const fuzzer = new MapFuzzer(map);
+      for (let j = 0; j < 1000; j++) fuzzer.runStep();
+    });
+  }
+});

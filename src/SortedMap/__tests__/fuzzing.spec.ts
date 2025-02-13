fd5dc4de35f3bbb9fad04a9525207f905bba0e9b
streamich <streamich@gmail.com>
2025-02-13 22:27:14 +0100

    test: 💍 update test

--- src/SortedMap/__tests__/fuzzing.spec.ts
+++ src/SortedMap/__tests__/fuzzing.spec.ts
@@ -1,10 +1,10 @@
 import {MapFuzzer} from '../../__tests__/MapFuzzer';
-import {AvlMap} from '../AvlMap';
+import {SortedMap} from '../SortedMap';
 
-describe('AvlMap fuzzing', () => {
+describe('red-black tree fuzzing', () => {
   for (let i = 0; i < 50; i++) {
     test(`map instance ${i}`, () => {
-      const map = new AvlMap<number, number>();
+      const map = new SortedMap<number, number>();
       const fuzzer = new MapFuzzer(map);
       for (let j = 0; j < 1000; j++) fuzzer.runStep();
     });
